6f4f52c52adde342ddde342e4ccd11cae3e6ee70
zhenf091 <35745610+zhenf091@users.noreply.github.com>
2018-05-18 16:52:54 -0400

    Initial commit

--- week-07/sketch.js
+++ week-07/sketch.js
@@ -1,27 +1,22 @@
-var spaceData;
+var weather;
 
 function setup(){
 	createCanvas(800,800);
-	loadJSON("http://api.open-notify.org/astros.json",gotData,'jsonp');
+	loadJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=f6bb96f9cf44837394a4502cb700e06f",gotData,'jsonp');
 }
 
 function gotData(data){
-	spaceData = data;
+	weather = data;
 
 }
 
 function draw(){
-	background(0,32,134);
-	if (spaceData){
-	  randomSeed(4);
-	  for (var i = 0, i < spaceData.number; i++){
-		fill(31,54,129);
-		elllipse(random(width),random(height),20,20);
+	background(0);
+	if (weather){
+		ellipse(100,100,weather.main.temp,weather.main.temp);
+		ellipse(100,100,weather.main.humidity,weather.main.humidity);
+		ellipse(100,100,weather.wind.speed,weather.wind.speed);
+		ellipse(100,100,weather.wind.deg,weather.wind.deg);
+		ellipse(100,100,sys.id,sys.id);
 	}
-}
-	stroke(123,231,34);
-	line(x, 0, x, height);
-	x = x + 1;
-	if (x > width){
-		x = 0;
 	}
