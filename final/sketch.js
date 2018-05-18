82e899160082c5cefb926503060162ad8da83c28
zhenf091 <35745610+zhenf091@users.noreply.github.com>
2018-05-18 14:31:37 -0400

    Initial commit

--- final/sketch.js
+++ final/sketch.js
@@ -3,10 +3,11 @@ var renderer, scene, camera, myCanvas = document.getElementById('myCanvas');
 //for scenes
 var sceneState = {
   INTRO: 0,
-  LAB: 1,
-  ZOOM: 2,
-  GALAXY: 3,
-  END: 4
+  TEXT: 1,
+  LAB: 2,
+  ZOOM: 3,
+  GALAXY: 4,
+  END: 5
 };
 
 var currentState = sceneState.INTRO;  //itgwl
@@ -17,6 +18,20 @@ var ZoomTimer;
 var ZookTimePressed;
 const timeForLab = 3000;
 const timeForZoom = 5000;
+
+
+//nasa
+
+// var url = "https://api.nasa.gov/planetary/apod?api_key=ylcqMbCTnxnOkaZtorFssBIUoOre4V2nRTrFklmj";
+
+
+// $.ajax({
+//   url: url,
+//   success: function(result){
+//   if("copyright" in result) {
+//     $("#copyright").text("Image Credits: " + result.copyright);
+//   }
+// });
 
 
 // function run() {
@@ -38,8 +53,8 @@ const timeForZoom = 5000;
     //old
     //camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000 );
     //new
-    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set(15,-20,100);
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.7, 600 );
+    camera.position.set(0,0,30);
     //SCENE
     scene = new THREE.Scene();
 
@@ -56,31 +71,39 @@ const timeForZoom = 5000;
 function drawScene(whichScene) {
   switch (currentState) {
     case sceneState.INTRO:
-
       break;
-    case sceneState.LAB:
+    case sceneState.TEXT:
     updateModel1();
       break;
-    case sceneState.ZOOM:
+    case sceneState.LAB:
     updateModel2();
       break;
-    case sceneState.GALAXY:
+    case sceneState.ZOOM:
     updateModel3();
+      break;
+    case sceneState.GALAXY:
+    updateModel4();
       break; 
     case sceneState.END:
-    updateModel4();
+    updateModel5();
     default:
       break;
   }
 }
 
 function checkTransition(whichScene) {
+
+  //check currentstate
   switch (whichScene) {
     case sceneState.INTRO:
     if (keyOn){
       setUpScene(currentState);
     }
-
+      break;
+    case sceneState.TEXT:
+        if (keyOn){
+      setUpScene(currentState);
+    }
       break;
     case sceneState.LAB:
         if (keyOn){
@@ -103,14 +126,18 @@ function checkTransition(whichScene) {
     }
 
       break;
+
+
     default:
       break;
   }
 }
-
+//giving it to the function above
 function setUpScene(whichScene) {
   switch (whichScene) {
     case sceneState.INTRO:
+      break;
+    case sceneState.TEXT:
       break;
     case sceneState.LAB:
       break;
@@ -136,14 +163,38 @@ function keyPressed() {
 
  }
 
- function updateModel1(){
+  function updateModel1(){
+//      var jsonModel = 'text.json';
+//     var loader = new THREE.JSONLoader();
+
+//     //callback function, two functions in the bracket
+//     function loadz(geometry, materials) {
+//         //BASIC MESH
+//         var material = new THREE.MeshNormalMaterial();
+//         var mesh = new THREE.Mesh(geometry, material);
+
+//         //adding the 3d obj
+//         scene.add(mesh);
+//         //lemme know where u at tho
+//         console.log(mesh.position);
+//          //mesh.position.x = 20;
+//          //mesh.position.z = 10;
+//          mesh.position.y = 4;
+//         //mesh have to be inside of the function.
+//     }
+// //callback function, two functions in the bracket
+//     loader.load(jsonModel, loadz);
+
+ }
+
+ function updateModel2(){
    //Model 
+   scene.remove(scene.children[2]);
     // var jsonModel = 'astro.json','class.json','gal.json','box.json';
     var jsonModel = 'class.json';
     var loader = new THREE.JSONLoader();
-
     //callback function, two functions in the bracket
-    function handle_load(geometry, materials) {
+    function loadz(geometry, materials) {
         //BASIC MESH
         var material = new THREE.MeshNormalMaterial();
         var mesh = new THREE.Mesh(geometry, material);
@@ -151,22 +202,23 @@ function keyPressed() {
         console.log(mesh.position);
          //mesh.position.x = 20;
          //mesh.position.z = 10;
-        // mesh.position.y = -100;
+         mesh.position.y = -10;
         //mesh have to be inside of the function.
     }
 //callback function, two functions in the bracket
-    loader.load(jsonModel, handle_load);
+    loader.load(jsonModel, loadz);
  }
 
- function updateModel2(){
+ function updateModel3(){
    //Model 
+   scene.remove(scene.children[2]);
     // var jsonModel = 'astro.json','class.json','gal.json','box.json';
     var jsonModel = 'box.json';
     scene.remove(scene.children[2]);
     var loader = new THREE.JSONLoader();
 
     //callback function, two functions in the bracket
-    function handle_load(geometry, materials) {
+    function loadz(geometry, materials) {
         //BASIC MESH
         var material = new THREE.MeshNormalMaterial();
         var mesh = new THREE.Mesh(geometry, material);
@@ -176,10 +228,11 @@ function keyPressed() {
         //mesh have to be inside of the function.
     }
 //callback function, two functions in the bracket
-    loader.load(jsonModel, handle_load);
+    loader.load(jsonModel, loadz);
  }
 
-  function updateModel3(){
+  function updateModel4(){
+    scene.remove(scene.children[2]);
    //Model 
     // var jsonModel = 'astro.json','class.json','gal.json','box.json';
     var jsonModel = 'gal.json';
@@ -187,50 +240,61 @@ function keyPressed() {
     var loader = new THREE.JSONLoader();
 
     //callback function, two functions in the bracket
-    function handle_load(geometry, materials) {
+    function loadz(geometry, materials) {
         //BASIC MESH
         var material = new THREE.MeshNormalMaterial();
         var mesh = new THREE.Mesh(geometry, material);
         scene.add(mesh);
         console.log(mesh.position);
-        mesh.position.z = -10;
+        mesh.position.y = -10;
         //mesh have to be inside of the function.
     }
 //callback function, two functions in the bracket
-    loader.load(jsonModel, handle_load);
+    loader.load(jsonModel, loadz);
  }
 
-  function updateModel4(){
+  function updateModel5(){
+    scene.remove(scene.children[2]);
    //Model 
     // var jsonModel = 'astro.json','class.json','gal.json','box.json';
+
+    //load json process of------
     var jsonModel = 'box.json';
-    scene.remove(scene.children[2]);
+
+    //delete
+   
+    //new loader for new json for model 5
     var loader = new THREE.JSONLoader();
 
     //callback function, two functions in the bracket
-    function handle_load(geometry, materials) {
+
+    //during loadz
+    function loadz(geometry, materials) {
         //BASIC MESH
+        //adding mesh/mesh related
         var material = new THREE.MeshNormalMaterial();
         var mesh = new THREE.Mesh(geometry, material);
         scene.add(mesh);
         console.log(mesh.position);
-        mesh.position.z = -10;
+        mesh.position.y = -10;
         //mesh have to be inside of the function.
     }
 //callback function, two functions in the bracket
-    loader.load(jsonModel, handle_load);
+
+//call at the same time json function and loadz at the same time.
+    loader.load(jsonModel, loadz);
  }
 
  function Pressed(){
     drawScene(currentState);
     keyOn = true;
-    if (currentState < 4){
+    if (currentState < 5){
       currentState ++;
       console.log(currentState);
-    } else {
-    if (currentState = 2){
-      console.log(currentState);
-    }
+    //  } else {
+    // if (currentState = 2){
+    //   console.log(currentState);
+    // }
     }
     
  }
