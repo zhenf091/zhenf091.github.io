22031d1db43ecce6f72180a9600eaba7e9abc4ba
zhenf091 <35745610+zhenf091@users.noreply.github.com>
2018-05-18 16:59:18 -0400

    Initial commit

---
+++ week-05/w5_02_choose_adventure_data/sketch.js
@@ -0,0 +1,69 @@
+// code 2
+// section a
+// bfa dt
+// spring 2018
+// bryan ma
+
+// week 5
+// choose your own adventure data
+
+// scene data model: 
+
+// {
+//   sceneText: '', //the scene text
+//   options: [], // the text options to choose
+//   nextScenes: []  // the target scene based on the previous options
+// }
+
+var sceneData;
+
+var currentScene = 0;
+var scenes = [];
+
+function preload() {
+  sceneData = loadJSON('scenes.json');
+}
+
+function setup() {
+  createCanvas(800, 800);
+  CreateScenesFromData(sceneData.scenes);
+}
+
+function draw() {
+  background(231, 190, 150);
+  scenes[currentScene].display();
+
+  fill(0);
+  textSize(24);
+  text("press the option number to advance to the indicated scene", 50, 700);
+}
+
+function CreateScenesFromData(data) {
+  for (var i = 0; i < data.length; i++) {
+    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes))
+  }
+}
+
+function Scene(sceneText, options, nextScenes) {
+  this.sceneText = sceneText;
+  this.options = options;
+  this.nextScenes = nextScenes;
+
+  this.display = function() {
+    fill(0);
+    textSize(32);
+    text(this.sceneText, 100, 100);
+
+    for (var i = 0; i < options.length; i++) {
+      text('OPTION ' + (i + 1) + ': ' + this.options[i], 150, 200 + i * 50);
+    }
+  }
+}
+
+function keyPressed() {
+  var numberPressed = parseInt(key);
+  var newScene = scenes[currentScene].nextScenes[numberPressed - 1];
+  if (newScene !== undefined) {
+    currentScene = newScene;
+  }
+}
