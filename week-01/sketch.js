622411765df6451b47cdc2dec8b24534f94ba98b
zhenf091 <35745610+zhenf091@users.noreply.github.com>
2018-05-18 18:53:21 -0400

    Initial commit

--- week-01/sketch.js
+++ week-01/sketch.js
@@ -6,5 +6,5 @@ function draw() {
   background(0,0,21);
   fill(212,12,32);
   textSize(24);
-  text("Processing stuff is up in Canvas", 50, 700);
+  text("Processing stuff is up in Canvas.", 50, 700);
 }
