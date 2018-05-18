96dfc628f4b58d176c30dcb50606f49e6708a9e8
zhenf091 <35745610+zhenf091@users.noreply.github.com>
2018-05-18 15:37:14 -0400

    Initial commit

---
+++ week-05/sketch.js
@@ -0,0 +1,78 @@
+// code 2
+// section a
+// bfa dt
+// spring 2018
+// bryan ma
+
+// week 5
+// saving/loading paint data
+// based on example by Jon Beilin
+
+var paintmarks = [];
+var paintDataFile = 'paintData.json';
+
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(255);
+  for (var i = 0; i < paintmarks.length; i++) {
+    paintmarks[i].display();
+  }
+
+  fill(0);
+  textSize(24);
+  text("drag the mouse across the canvas to draw.", 50, 570);
+  text("press 'S' to save a json file with the current paint data.", 50, 600);
+  text("press 'L' to load a json file from your computer.", 50, 630);
+}
+
+function PaintMark(position) {
+  this.position = position;
+
+  this.display = function() {
+    noStroke();
+    fill(250,0,250);
+    ellipse(this.position.x, this.position.y, 10, 10);
+  }
+
+}
+
+function mouseDragged() {
+  paintmarks.push(new PaintMark(createVector(mouseX, mouseY)));
+}
+
+function keyPressed() {
+  if (key === 'S') {
+    savePaintData();
+  }
+  if (key === 'L') {
+    loadPaintData();
+  }
+}
+
+function savePaintData() {
+  positionsToSave = [];
+  for (var i = 0; i < paintmarks.length; i++) {
+    positionsToSave.push(
+      { 
+        x: paintmarks[i].position.x, 
+        y: paintmarks[i].position.y 
+      }
+    );
+  }
+  saveJSON(positionsToSave, 'paintData.json');
+}
+
+function loadPaintData() {
+  loadJSON(paintDataFile, parsePaintData);
+}
+
+function parsePaintData(data) {
+  paintmarks = [];
+
+  for (var i = 0; i < data.length; i++) {
+    paintmarks.push(new PaintMark(createVector(data[i].x, data[i].y)));
+  }
+}
